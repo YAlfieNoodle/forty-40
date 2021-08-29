b88082bac0076c8d1742148771495572a596bd5f
YAlfieNoodle <77399222+YAlfieNoodle@users.noreply.github.com>
2021-08-29 13:53:03 +0100

    Create index.py

---
+++ index.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template,request, session,redirect
+from threading import Thread
+from oauth import Oauth
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+	return redirect("youtube.com")
+
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
